505c9012635d3357ac59e684067ec7afb0c0880d
haibo.jiang <jianghaibo1985@gmail.com>
2021-03-17 22:48:41 +1300

    center footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,23 @@ import styles from "./Footer.module.scss";
 import logoBig from "../images/logo-big.png";
 import facebookLike from "../images/button-facebook-on.gif";
 import classnames from "classnames";
+import { Link } from "gatsby";
 
 export default function Footer() {
   return (
     <footer className={classnames(styles.Footer, "container")}>
       <hr />
       <div className="columns is-vcentered">
-        <div className="column is-centered">
+        <div className="column is-centered has-text-centered">
           <div>Phone 64 3372 3537</div>
-          <div>Click here to email us online</div>
+          <div>
+            <Link to="/book">Click here to email us online</Link>
+          </div>
         </div>
-        <div className="column">
+        <div className="column has-text-centered">
           <img src={logoBig} alt="te waipounamu" />
         </div>
-        <div className="column">
+        <div className="column has-text-centered">
           <a
             href="https://www.facebook.com/motorcycletours.nz/"
             target="_blank"
