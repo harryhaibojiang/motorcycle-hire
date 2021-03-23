e2b36142637fd8e5c55b9836bdb06e5c62230878
haibo.jiang <jianghaibo1985@gmail.com>
2021-03-24 10:57:20 +1300

    Rename siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
       "Hire from New Zealand’s largest most caring &amp; professional Motorcycle Rental Company. New Fleet of Latest model BMW R1200GS, R1200RT, F800GS F700GS, G650GS. Triumph Tiger 800 &amp; 1050, Suzuki V-Strom &amp; other adventure bikes, all expertly prepared for your tour.",
     image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
     twitterUsername: "@occlumency",
-    url: "https://www.motorcycle-hire.co.nz", // No trailing slash allowed!
+    siteUrl: "https://www.motorcycle-hire.co.nz", // No trailing slash allowed!
   },
   plugins: [
     `gatsby-plugin-transition-link`,
