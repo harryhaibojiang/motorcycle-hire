6d79fed71cb90601283054b1f8268b27639d4aad
haibo.jiang <harryhaibojiang@gmail.com>
2022-12-12 10:24:41 +1300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: "New Zealand Motorcycle Tours, Rentals and Hire NZ",
     titleTemplate: "%s - Rental Motorbikes Te Waipounamu Motorcycle Tours",
     description:
-      "Hire from New Zealand’s largest most caring &amp; professional Motorcycle Rental Company. New Fleet of Latest model BMW R1200GS, R1200RT, F800GS F700GS, G650GS. Triumph Tiger 800 &amp; 1050, Suzuki V-Strom &amp; other adventure bikes, all expertly prepared for your tour.",
+      "Hire from New Zealand’s largest most caring and professional Motorcycle Rental Company. New Fleet of Latest model BMW R1250GS, R1250RT, F850GS F750GS. Triumph Tiger 800, Suzuki V-Strom, and other adventure bikes are all expertly prepared for your tour.",
     image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
     twitterUsername: "@occlumency",
     siteUrl: "https://www.motorcycle-hire.co.nz", // No trailing slash allowed!
